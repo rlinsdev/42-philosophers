5fd9a0d8489a372436e066077e4ddf4c03fc667d
Rodrigo Lins <rlinsdev@gmail.com>
2023-02-03 16:51:53 -0300

    Norminette

--- README.md
+++ README.md
@@ -80,6 +80,3 @@ $ ./philo 5	86400000 3600000 28800000 2
 
 
 
-TODO: Test with a lot of philo - simulate a lot of threads together - veify no erro (DEad lock / data racing)
-
-TODO: Will be better get number in forks, not Right or left fork
--- philo/time.c
+++ philo/time.c
@@ -6,17 +6,18 @@
 /*   By: rlins <rlins@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 10:20:01 by rlins             #+#    #+#             */
-/*   Updated: 2023/02/03 12:05:06 by rlins            ###   ########.fr       */
+/*   Updated: 2023/02/03 16:51:25 by rlins            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int datetime_now()
+int	datetime_now(void)
 {
-	struct timeval time;
+	struct timeval	time;
+
 	gettimeofday(&time, NULL);
-	return time.tv_sec * 1000 + time.tv_usec / 1000;
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
 int	get_time_ms(int start_dinning)
@@ -24,9 +25,9 @@ int	get_time_ms(int start_dinning)
 	return (datetime_now() - start_dinning);
 }
 
-void thread_sleep(t_table *table, time_t duration)
+void	thread_sleep(t_table *table, time_t duration)
 {
-	time_t time_wake_up;
+	time_t	time_wake_up;
 
 	time_wake_up = datetime_now() + duration;
 	while (datetime_now() < time_wake_up)
